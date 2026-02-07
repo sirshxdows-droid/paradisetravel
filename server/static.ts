d3c7dbc574c0a88d07c077f4841897bf5b89db3c
sirshxdows-droid <sirshxdows@gmail.com>
2026-02-07 19:27:47 +0000

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -1,19 +1,15 @@
-import express, { type Express } from "express";
-import fs from "fs";
-import path from "path";
-
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  const distPath = path.resolve(process.cwd(), "dist/public");
+
   if (!fs.existsSync(distPath)) {
     throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
+      `Could not find the build directory: ${distPath}. Make sure the client is built.`,
     );
   }
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+    res.sendFile(path.join(distPath, "index.html"));
   });
 }
